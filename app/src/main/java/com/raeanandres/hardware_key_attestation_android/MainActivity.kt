ff93e6a09a41f761e919bfe303baa8c433b36da4
Rae-An Andres <raeandres@users.noreply.github.com>
2026-01-28 11:57:53 +0800

    init commit

---
+++ app/src/main/java/com/raeanandres/hardware_key_attestation_android/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.raeanandres.hardware_key_attestation_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.raeanandres.hardware_key_attestation_android.ui.theme.HardwarekeyattestationandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HardwarekeyattestationandroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HardwarekeyattestationandroidTheme {
+        Greeting("Android")
+    }
+}
